72aaedefb51912eccd9f22a9f8bf52d113d1346d
nolanborz <nolanrborzoni@gmail.com>
2025-02-12 15:16:37 +0900

    Take some countermeasures against fisherman

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -41,7 +41,7 @@ class LineItemsController < ApplicationController
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item.cart, notice: "Line item was successfully updated." }
+        format.html { redirect_to @line_item, notice: "Line item was successfully updated." }
         format.json { render :show, status: :ok, location: @line_item }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -68,6 +68,6 @@ class LineItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -42,13 +42,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(@product)
   end
 
-  test "should destroy product" do
-    assert_difference("Product.count", -1) do
-      delete product_url(@product)
-    end
 
-    assert_redirected_to products_url
-  end
 
   test "should destroy product" do
     assert_raises ActiveRecord::RecordNotDestroyed do
